03b06715baf51e79d58ee83609651f315da508bf
tibetdev <tenzinkunchok2022@gmail.com>
2024-07-25 09:04:07 +0200

    inital commit

--- components/ToolDetails.jsx
+++ components/ToolDetails.jsx
@@ -44,10 +44,7 @@ export default function ToolDetails({ toolData, githubData, readmeData }) {
 
   return (
     <div className='px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20'>
-      <Head>
-        {/* <script src='//katacoda.com/embed.js'></script> */}
-        {/* not working  */}
-      </Head>
+      <Head>{/* <script src='//katacoda.com/embed.js'></script> */}</Head>
       <div className='grid grid-cols-1 lg:grid-cols-3 gap-2.5 lg:gap-5 mb-5'>
         <div className='lg:col-span-2'>
           <h2 className='text-2xl lg:text-4xl font-bold mb-2'>{name}</h2>
