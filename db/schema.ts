27a0751b2107242c45ed99b8a0345ef842bc0022
tibetdev <tenzinkunchok2022@gmail.com>
2024-07-30 23:38:45 +0200

    updated database schema

--- db/schema.ts
+++ db/schema.ts
@@ -5,11 +5,15 @@ export const usersTable = pgTable('users_table', {
   name: text('name').notNull(),
   age: integer('age').notNull(),
   email: text('email').notNull().unique(),
+  password: text('password').notNull(),
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+  updatedAt: timestamp('updated_at')
+    .notNull()
+    .$onUpdate(() => new Date()),
 });
 
-export const postsTable = pgTable('posts_table', {
+export const commentTable = pgTable('posts_table', {
   id: serial('id').primaryKey(),
-  title: text('title').notNull(),
   content: text('content').notNull(),
   userId: integer('user_id')
     .notNull()
@@ -23,5 +27,5 @@ export const postsTable = pgTable('posts_table', {
 export type InsertUser = typeof usersTable.$inferInsert;
 export type SelectUser = typeof usersTable.$inferSelect;
 
-export type InsertPost = typeof postsTable.$inferInsert;
-export type SelectPost = typeof postsTable.$inferSelect;
+export type InsertPost = typeof commentTable.$inferInsert;
+export type SelectPost = typeof commentTable.$inferSelect;
